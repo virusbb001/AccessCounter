418145ee21db3aa0c45c216ba155c253c8897230
virus <virusbb001a@gmail.com>
2014-04-10 16:56:09 +0900

    設定項目追加 間隔設定追加 保存するカウント数を追加 addCountを追加 refleshCountを追加 readData,writeDataを追加 その他もろもろ

---
+++ lib/AccessCounter.pm
@@ -0,0 +1,115 @@
+package AccessCounter;
+
+use Carp;
+use JSON::XS qw/encode_json decode_json/;
+
+sub new{
+ my $pkg=shift;
+ # 設定に必要なもの
+ # セーブ先ファイル(ハンドラ)
+ # 合計カウントを保存するか
+ # 何日おきにリセットするか
+ # どのくらい保存するか
+ my $config=shift;
+ if(!defined($config->{saveFile})){
+  croak("saveFile is not defined");
+ }
+ my %defaultConfig=(
+  # 合計カウントを別に保存するか
+  saveTotal => 1,
+  # n日分あけてカウントをする 
+  # 負数 ならTOTALのみ 
+  # 0なら次の日に(デフォルトは0)
+  # 1なら今日,明日でまとめて, 明後日,4日後にまとめてカウントをする
+  countInterval => 0,
+  # 何回分保存するか
+  saveCount => 2
+ );
+ my %conf=(%defaultConfig,%$config);
+ my $ref=\%conf;
+ bless ( $ref,$pkg);
+ return $ref;
+}
+
+# カウント追加
+# データのリセットなどもここで行う
+# 基本はこの関数だけ使う
+sub addCount{
+ my $this=shift;
+ my $data=$this->readData();
+ $this->refleshData($data);
+ $data->{total}++;
+ $data->{day}->[0]++;
+ $data->{updateTime}=int(time()/86400);
+ $this->writeData($data);
+}
+
+# データ読み込み
+sub readData{
+ my $this=shift;
+ my $fh=$this->{saveFile};
+ my (@text,$text);
+ my $data;
+ seek($fh,0,0);
+ @text=<$fh>;
+ $text=join("",@text);
+ if(length($text)!=0){
+  eval{
+   $data=decode_json($text);
+  };
+  if($@){
+   carp($text,$@);
+  }
+ }else{
+  $data={
+   updateTime=>time()
+  };
+  if($this->{"saveTotal"} == 1){
+   $data->{"total"}=0;
+  }
+  if($this->{"countInterval"} >= 0 ){
+   $data->{"day"}=[];
+   for(my $i=0; $i<$this->{"saveCount"};$i++){
+    $data->{"day"}->[$i]=0;
+   }
+  }
+  # 空の時にデータ初期化, 上書き
+
+  $this->writeData($data);
+ }
+
+ return $data;
+}
+
+# データ書き出し
+# 無理やり書き換えるときとか
+sub writeData{
+ my $this=shift;
+ my $data=shift;
+ my $text;
+ my $fh=$this->{saveFile};
+ seek($fh,0,0);
+ truncate($fh,0);
+ if(defined $data){
+  $text=encode_json($data);
+ }
+ print $fh $text;
+}
+
+# データ更新
+sub refleshData{
+ my $this=shift;
+ my $data=shift;
+ if(!defined($data)){
+  $data=$this->readData();
+ }
+ my $interval=int(time/86400) - $data->{updateTime};
+ my $histnum=int($interval/($countInterval+1));
+ for(my $i=0;$i<$histnum;$i++){
+  unshift(@{$data->{day}},0);
+ }
+ splice(@{$data->{day}},$this->{saveCount});
+ return $data;
+}
+
+1;
---
+++ t/01_use.t
@@ -0,0 +1,64 @@
+use strict;
+use warnings;
+
+use Test::More tests => 8;
+use Test::MockTime qw(:all);
+use File::Temp qw/tempfile/;
+use AccessCounter;
+use Data::Dumper;
+
+my ($tempfile,$tempfilename)=tempfile();
+
+set_fixed_time(0);
+# よくある昨日, 今日, 合計のカウンター
+my $accessCounter=AccessCounter->new({
+  saveFile => $tempfile,
+  saveCount => 2
+ });
+
+ok(defined $accessCounter , "AccessCounter->new returned something");
+ok($accessCounter ->isa('AccessCounter')," and it's AccessCounter Class");
+
+ok(defined($accessCounter->{saveFile}),"savefile is defined");
+is($accessCounter->{saveTotal} , 1,"saveTotal must be 1 because it is default");
+
+subtest 'first Read Data Test' => sub{
+ my $data=$accessCounter->readData();
+ ok($data->{total} == 0,"total counter must be 0");
+ ok($data->{day}->[0] == 0,"today counter must be 0");
+ ok($data->{day}->[1] == 0,"yesterday counter must be 0");
+};
+
+set_fixed_time(50);
+$accessCounter->addCount();
+subtest 'first Day add count Test' => sub{
+ my $data=$accessCounter->readData();
+
+ is($data->{total}, 1,"total counter must be 1");
+ is($data->{day}->[0] , 1,"today counter must be 1");
+ is($data->{day}->[1] , 0,"yesterday counter must be 0");
+ is($data->{updateTime},int(time()/86400),"updateTime must be today but not now");
+};
+
+set_fixed_time(86400);
+subtest '2nd Day read Test (with reflesh)' => sub{
+ my $data=$accessCounter->refleshData();
+
+ is($data->{total}, 1,"total counter must be 1");
+ is($data->{day}->[0] , 0,"today counter must be 0");
+ is($data->{day}->[1] , 1,"yesterday counter must be 1");
+ is($data->{updateTime},int(time()/86400-1),"updateTime must be yesterday");
+};
+
+$accessCounter->addCount();
+subtest '2nd Day add Test' => sub{
+ my $data=$accessCounter->refleshData();
+
+ is($data->{total}, 2,"total counter must be 2");
+ is($data->{day}->[0] , 1,"today counter must be 1");
+ is($data->{day}->[1] , 1,"yesterday counter must be 1");
+ is($data->{updateTime},int(time()/86400-1),"updateTime must be yesterday");
+};
+
+
+close($tempfile);
